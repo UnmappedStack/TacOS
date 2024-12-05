9289119ccbecfffcb2a70e23e20e2eb205967832
UnmappedStack <jake.steinburger0@gmail.com>
2024-12-06 10:04:31 +1100

    Implement printf

--- include/string.h
+++ include/string.h
@@ -1,7 +1,12 @@
 #pragma once
+#include <stdint.h>
 #include <stddef.h>
 
 size_t strlen(const char *str);
 void *memset (void *dest, int x, size_t n);
 char *strcpy(char *dest, const char *src);
 void *memcpy (void *dest, const void *src, size_t n);
+void uint64_to_binary_string(uint64_t num, char *buf);
+void uint64_to_hex_string(uint64_t num, char *str);
+void uint64_to_string(uint64_t num, char* str);
+void uint64_to_hex_string_padded(uint64_t num, char *str);
--- src/entry.c
+++ src/entry.c
@@ -1,8 +1,11 @@
 #include <serial.h>
+#include <printf.h>
 
 void _start() {
     write_serial("Hello from serial! This is TacOS, my latest OS kernel.\n"
                  "Every OS I write is a little better. SpecOS was terrible. PotatOS was better but still kinda broken.\n"
                  "This one, I wanna make genuinely the best one I've ever done.\n");
+    printf("Printf test - Number: %i, binary literal: %b, hex literal: %x, string: %s\n",
+            69, 0b10011001, 0x42, "hi there");
     for (;;);
 }
