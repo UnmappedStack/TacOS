9b13a35493b97135682adf96e11f6ee3ce809a87
UnmappedStack <jake.steinburger0@gmail.com>
2024-12-18 22:05:32 +1100

    interrupts

--- include/kernel.h
+++ include/kernel.h
@@ -1,10 +1,12 @@
 #pragma once
 #include <stdint.h>
+#include <cpu/idt.h>
 #include <cpu/gdt.h>
 
 typedef struct {
     uint64_t GDT[5];
     GDTR     gdtr;
+    IDTEntry IDT[256];
 } Kernel;
 
 extern Kernel kernel;
--- src/entry.c
+++ src/entry.c
@@ -1,6 +1,7 @@
 #include <kernel.h>
 #include <serial.h>
 #include <cpu/gdt.h>
+#include <cpu/idt.h>
 #include <printf.h>
 
 Kernel kernel = {0};
@@ -8,5 +9,7 @@ Kernel kernel = {0};
 void _start() {
     init_serial();
     init_GDT();
+    init_IDT();
+    asm("int $0x80");
     for (;;);
 }
