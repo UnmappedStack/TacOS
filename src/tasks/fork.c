998fd22178e1b9e9ea87d56a8f2cb5c9b940c464
UnmappedStack <jake.steinburger0@gmail.com>
2025-01-02 22:49:23 +1100

    fork() returns twice

--- src/tasks/fork.c
+++ src/tasks/fork.c
@@ -3,12 +3,12 @@
 #include <kernel.h>
 
 pid_t fork() {
-    Task *new_task   = task_add();
-    new_task->entry  = kernel.scheduler.current_task->entry;
-    new_task->parent = kernel.scheduler.current_task;
-    new_task->flags  = kernel.scheduler.current_task->flags;
-    new_task->pml4   = (uint64_t) init_paging_task();
-    bool is_first    = true;
+    const Task *initial_task = kernel.scheduler.current_task;
+    Task *new_task     = task_add();
+    new_task->entry    = kernel.scheduler.current_task->entry;
+    new_task->parent   = kernel.scheduler.current_task;
+    new_task->pml4     = (uint64_t) init_paging_task();
+    bool is_first      = true;
     for (struct list *iter = &kernel.scheduler.current_task->memregion_list->list;
          iter != &kernel.scheduler.current_task->memregion_list->list || is_first;
          iter = iter->next
@@ -19,5 +19,18 @@ pid_t fork() {
                         new_task->pml4 + kernel.hhdm
         );
     }
-    return new_task->pid;
+    new_task->flags = kernel.scheduler.current_task->flags; /* Flags are set last so that it's only 
+                                                             * ever run after everything else is set up
+                                                             * (because of the TASK_PRESENT flag) */
+    // TODO: Remove debug messages once everything is *definitely* working
+    if (kernel.scheduler.current_task->pid == initial_task->pid) {
+        printf("Parent\n");
+        return new_task->pid;
+    } else if (kernel.scheduler.current_task->pid == new_task->pid) {
+        printf("Child\n");
+        return 0;
+    } else {
+        printf("Weird as fuck error in fork(), wtf??? (supposed to be unreachable but theoretically reachable)\n");
+        return 0;
+    }
 }
