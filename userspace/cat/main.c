8e9451301a05dc517e16075a326e86c8d011fa9e
UnmappedStack <jake.steinburger0@gmail.com>
2025-04-20 19:25:10 +1000

    implement cat

---
+++ userspace/cat/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        printf("cat only takes one argument, being the file to read from.\n");
+        return -1;
+    }
+    int f = open(argv[1], 0, 0);
+    if (!f) {
+        printf("Failed to open %s\n", argv[1]);
+        return -1;
+    }
+    off_t sz = lseek(f, 0, SEEK_END);
+    lseek(f, 0, SEEK_SET);
+    char *buf = (char*) malloc(sz);
+    read(f, buf, sz);
+    puts(buf);
+    close(f);
+    return 0;
+}
