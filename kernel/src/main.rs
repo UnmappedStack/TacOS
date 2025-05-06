d69022afb12302f1bada892586bb98463c4474af
UnmappedStack <jake.steinburger0@gmail.com>
2025-05-07 07:39:32 +1000

    make the rust panic handler actually print the issue

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -9,13 +9,13 @@ use drivers::serial;
 
 #[unsafe(no_mangle)]
 unsafe extern "C" fn kmain() -> ! {
-    assert!(bootloader::BASE_REVISION.is_supported());
+    assert!(!bootloader::BASE_REVISION.is_supported());
     serial::init_serial();
     cpu::halt_device();
 }
 
 #[panic_handler]
-fn rust_panic(_info: &core::panic::PanicInfo) -> ! {
-    println!("PANIC");
+fn rust_panic(info: &core::panic::PanicInfo) -> ! {
+    println!("Rust Panic: {}", info);
     cpu::halt_device();
 }
