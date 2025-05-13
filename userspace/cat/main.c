0f72c5bcd713b6fe13d6f443785cf5be72ac5cf8
UnmappedStack <jake.steinburger0@gmail.com>
2025-05-13 15:06:48 +1000

    [userspace] fix cat command to be more posixy

--- userspace/cat/main.c
+++ userspace/cat/main.c
@@ -3,8 +3,23 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+void cat_file(char **argv, int f) {
+    if (f < 0) {
+        printf("cat: Failed to open file: %s\n", argv[1]);
+        exit(-1);
+    }
+    off_t sz = lseek(f, 0, SEEK_END);
+    lseek(f, 0, SEEK_SET);
+    sz = (sz) ? sz : 256;
+    char *buf = (char*) malloc(sz);
+    read(f, buf, sz);
+    buf[sz] = 0;
+    if (sz) printf("\n");
+    puts(buf);
+}
+
 int main(int argc, char **argv) {
-    if (argc != 2) {
+    if (argc > 2) {
         printf("cat only takes one argument, being the file to read from.\n");
         return -1;
     }
@@ -14,17 +29,9 @@ int main(int argc, char **argv) {
                "See LICENSE in the source repo for more information.\n");
         exit(-1);
     }
-    int f = open(argv[1], 0, 0);
-    if (f < 0) {
-        printf("cat: Failed to open file: %s\n", argv[1]);
-        return -1;
-    }
-    off_t sz = lseek(f, 0, SEEK_END);
-    lseek(f, 0, SEEK_SET);
-    char *buf = (char*) malloc(sz);
-    read(f, buf, sz);
-    buf[sz] = 0;
-    puts(buf);
-    close(f);
+    int f = (argc > 1) ? open(argv[1], 0, 0) : fileno(stdin);
+    for (int i = 0; i < 1 || argc <= 1; i++)
+        cat_file(argv, f);
+    if (f != fileno(stdin)) close(f);
     return 0;
 }
