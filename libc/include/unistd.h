9dee3c097d837cf1addf8f72448b5857fb693484
UnmappedStack <jake.steinburger0@gmail.com>
2025-05-23 10:21:52 +1000

    [libc] execl

--- libc/include/unistd.h
+++ libc/include/unistd.h
@@ -11,5 +11,6 @@ off_t lseek(int fd, size_t offset, int whence);
 pid_t fork(void);
 int execve(const char *path, char **argv, char **envp);
 int execvp(const char *path, char **argv);
+int execl(const char *path, const char *arg0, ...);
 int chdir(const char *path);
 char *getcwd(char *buf, size_t size);
