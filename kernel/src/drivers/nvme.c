a4a197096ca65a14254cf38b8abf000a07ea4d5e
jakeSteinburger <jake@kendasi.com>
2025-07-10 14:57:07 +1000

    [nvme] create queues

--- kernel/src/drivers/nvme.c
+++ kernel/src/drivers/nvme.c
@@ -1,4 +1,5 @@
 #include <printf.h>
+#include <mem/pmm.h>
 #include <kernel.h>
 #include <stdint.h>
 #include <pci.h>
@@ -11,7 +12,14 @@ void nvme_interrupt_handler(void*) {
 }
 
 typedef struct {
+    uintptr_t addr;
+    uint64_t sz;
+} NVMeQueue;
+
+typedef struct {
     uintptr_t base;
+    NVMeQueue submission_queue;
+    NVMeQueue completion_queue;
 } NVMeDevice;
 
 NVMeDevice nvme_dev = {0};
@@ -23,6 +31,11 @@ uint32_t nvme_read_reg(uint32_t offset) {
 
 void nvme_write_reg(uint32_t offset, uint32_t value) {
 	volatile uint32_t *ptr = (volatile uint32_t *)(nvme_dev.base + offset);
+	*ptr = value;
+}
+
+void nvme_write_reg64(uint32_t offset, uint64_t value) {
+	volatile uint64_t *ptr = (volatile uint64_t *)(nvme_dev.base + offset);
 	*ptr = value;
 }
 
@@ -46,6 +59,18 @@ static void nvme_list_capabilities(uint64_t capabilities) {
     printf("Doorbell Stride               (DSTRT): 0x%x\n", dstrt);
 }
 
+static void create_submission_queue(NVMeQueue *queue) {
+    queue->addr = kmalloc(1) + kernel.hhdm;
+    queue->sz   = 63;
+    nvme_write_reg64(0x28, queue->addr);
+}
+
+static void create_completion_queue(NVMeQueue *queue) {
+    queue->addr = kmalloc(1) + kernel.hhdm;
+    queue->sz   = 63;
+    nvme_write_reg64(0x30, queue->addr);
+}
+
 // it occurs to me it could be problematic that this is using one single IDT vector,
 // so if there are multiple NVMe devices, then it would be overwritten each time. (TODO)
 void nvme_init(uint8_t bus, uint8_t device, uint8_t function, uint8_t capabilities_list) {
@@ -56,6 +81,7 @@ void nvme_init(uint8_t bus, uint8_t device, uint8_t function, uint8_t capabiliti
         printf("NVMe could not be initiated.\n");
         return;
     }
+
     uint32_t version = nvme_read_reg(0x8);
     uint8_t version_patch  = (uint8_t) version;
     uint8_t version_minor  = (uint8_t) (version >> 8);
@@ -63,6 +89,9 @@ void nvme_init(uint8_t bus, uint8_t device, uint8_t function, uint8_t capabiliti
     printf("NVMe version: %i.%i.%i\n", version_major, version_minor, version_patch);
     uint64_t capabilities = (uint64_t) nvme_read_reg(0x0) | ((uint64_t) nvme_read_reg(0x4) << 32);
     nvme_list_capabilities(capabilities);
+
+    create_submission_queue(&nvme_dev.submission_queue);
+    create_completion_queue(&nvme_dev.completion_queue);
     printf("NVMe device initiated.\n");
 }
 
