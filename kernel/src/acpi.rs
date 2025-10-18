3c176c209dd48b22646465b0e064ca12013050c2
UnmappedStack <jake.steinburger0@gmail.com>
2025-10-18 15:17:18 +1100

    [acpi] stub out rest of kernel api trait

--- kernel/src/acpi.rs
+++ kernel/src/acpi.rs
@@ -1,5 +1,6 @@
 use crate::{kernel::KERNEL, cpu};
-use alloc::sync::Arc;
+use alloc::{sync::Arc, boxed::Box};
+use core::ffi::c_void;
 
 const BYTE0_MASK: u32 = 0b1111;
 const BYTE1_MASK: u32 = BYTE0_MASK << 8;
@@ -95,6 +96,94 @@ impl uacpi::kernel_api::KernelApi for AcpiKernelApi {
             cpu::outd(0xCFC, to_write);
         }
         Ok(())
+    }
+    unsafe fn io_map(&self, _base: uacpi::IOAddr, _len: usize) -> Result<uacpi::Handle, uacpi::Status> {
+        todo!("uacpi: io_map");
+    }
+    unsafe fn io_unmap(&self, _handle: uacpi::Handle) {
+        todo!("uacpi: io_unmap");
+    }
+    unsafe fn io_read(&self, _handle: uacpi::Handle, _offset: usize, _byte_width: u8)
+                                                -> Result<u64, uacpi::Status> {
+        todo!("uacpi: io_read");
+    }
+    unsafe fn io_write(&self, _handle: uacpi::Handle, _offset: usize, _byte_width: u8, _val: u64)
+                                                -> Result<(), uacpi::Status> {
+        todo!("uacpi: io_write");
+    }
+    unsafe fn map(&self, _phys: uacpi::PhysAddr, _len: usize) -> *mut c_void {
+        todo!("uacpi: map");
+    }
+    unsafe fn unmap(&self, _addr: *mut c_void, _len: usize) {
+        todo!("uacpi: unmap");
+    }
+    fn get_ticks(&self) -> u64 {
+        todo!("uacpi: get_ticks");
+    }
+    fn stall(&self, _usec: u8) {
+        todo!("uacpi: stall");
+    }
+    fn sleep(&self, _msec: u8) {
+        todo!("uacpi: sleep");
+    }
+    fn create_mutex(&self) -> uacpi::Handle {
+        todo!("uacpi: create_mutex");
+    }
+    fn destroy_mutex(&self, _mutex: uacpi::Handle) {
+        todo!("uacpi: destry_mutex");
+    }
+    fn acquire_mutex(&self, _mutex: uacpi::Handle, _timeout: u16) -> bool {
+        todo!("uacpi: acquire_mutex");
+    }
+    fn release_mutex(&self, _mutex: uacpi::Handle) {
+        todo!("uacpi: release_mutex");
+    }
+    fn create_spinlock(&self) -> uacpi::Handle {
+        todo!("uacpi: create_spinlock");
+    }
+    fn destroy_spinlock(&self, _lock: uacpi::Handle) {
+        todo!("uacpi: destroy_spinlock");
+    }
+    fn acquire_spinlock(&self, _lock: uacpi::Handle) -> uacpi::CpuFlags {
+        todo!("uacpi: acquire_spinlock");
+    }
+    fn release_spinlock(&self, _lock: uacpi::Handle, _cpu_flags: uacpi::CpuFlags) {
+        todo!("uacpi: release_spinlock");
+    }
+    fn create_event(&self) -> uacpi::Handle {
+        todo!("uacpi: create_event");
+    }
+    fn destroy_event(&self, _event: uacpi::Handle) {
+        todo!("uacpi: destroy_event");
+    }
+    fn wait_for_event(&self, _event: uacpi::Handle, _timeout: u16) -> bool {
+        todo!("uacpi: wait_for_event");
+    }
+    fn signal_event(&self, _event: uacpi::Handle) {
+        todo!("uacpi: signal_event");
+    }
+    fn reset_event(&self, _event: uacpi::Handle) {
+        todo!("uacpi: reset_event");
+    }
+    fn get_thread_id(&self) -> uacpi::ThreadId {
+        todo!("uacpi: get_thread_id");
+    }
+    fn firmware_request(&self, _req: uacpi::FirmwareRequest) -> Result<(), uacpi::Status> {
+        todo!("uacpi: firmware_request");
+    }
+    fn install_interrupt_handler(&self, _irq: u32, _handler: Box<dyn Fn()>)
+                                                -> Result<uacpi::Handle, uacpi::Status> {
+        todo!("uacpi: install_interrupt_handler");
+    }
+    fn uninstall_interrupt_handler(&self, _handle: uacpi::Handle) -> Result<(), uacpi::Status> {
+        todo!("uacpi: uninstall_interrupt_handler");
+    }
+    fn schedule_work(&self, _work_type: uacpi::WorkType, _handler: Box<dyn Fn()>)
+                                                -> Result<(), uacpi::Status> {
+        todo!("uacpi: schedule_work");
+    }
+    fn wait_for_work_completion(&self) -> Result<(), uacpi::Status> {
+        todo!("uacpi: wait_for_work_completion");
     }
 }
 
