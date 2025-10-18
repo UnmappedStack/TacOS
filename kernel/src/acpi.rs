cd7a34522c6591337a1d3ed2c2a30ddb2395c61c
UnmappedStack <jake.steinburger0@gmail.com>
2025-10-18 14:13:02 +1100

    [acpi] pci_[read|write] impl for uacpi kernel api trait

--- kernel/src/acpi.rs
+++ kernel/src/acpi.rs
@@ -1,6 +1,13 @@
 use crate::{kernel::KERNEL, cpu};
 use alloc::sync::Arc;
 
+const BYTE0_MASK: u32 = 0b1111;
+const BYTE1_MASK: u32 = BYTE0_MASK << 8;
+const BYTE2_MASK: u32 = BYTE0_MASK << 16;
+const BYTE3_MASK: u32 = BYTE0_MASK << 24;
+
+const BYTE_MASKS: [u32; 4] = [BYTE0_MASK, BYTE1_MASK, BYTE2_MASK, BYTE3_MASK];
+
 struct AcpiKernelApi;
 
 impl uacpi::kernel_api::KernelApi for AcpiKernelApi {
@@ -51,6 +58,43 @@ impl uacpi::kernel_api::KernelApi for AcpiKernelApi {
                 _ => Err(uacpi::Status::InvalidArgument),
             }
         }
+    }
+    unsafe fn pci_read(&self, address: uacpi::PCIAddress, offset: usize, byte_width: u8)
+                                                -> Result<u64, uacpi::Status> {
+        if byte_width > 4 { return Err(uacpi::Status::InvalidArgument); }
+        let offset_aligned = (offset & !0b11) as u32;
+        let bus  = address.bus()      as u32;
+        let dev  = address.device()   as u32;
+        let func = address.function() as u32;
+        let addr = (bus << 16) | (dev << 11) | 
+            (func << 8) | (offset_aligned & 0xFC) | (0x80000000) as u32;
+        unsafe {
+            cpu::outd(0xCF8, addr);
+            let read_back = cpu::ind(0xCFC);
+            let ret = (read_back >> (byte_width-1*8)) & 0b1111;
+            Ok(ret as u64)
+        }
+    }
+    unsafe fn pci_write(&self, address: uacpi::PCIAddress, offset: usize, byte_width: u8, val: u64)
+                                                -> Result<(), uacpi::Status> {
+        if byte_width > 4 { return Err(uacpi::Status::InvalidArgument); }
+        let offset_aligned = (offset & !0b11) as u32;
+        let bus  = address.bus()      as u32;
+        let dev  = address.device()   as u32;
+        let func = address.function() as u32;
+        let addr = (bus << 16) | (dev << 11) | 
+            (func << 8) | (offset_aligned & 0xFC) | (0x80000000) as u32;
+        unsafe {
+            // read to get current value
+            cpu::outd(0xCF8, addr);
+            let current = cpu::ind(0xCFC);
+            // write back
+            let mask = BYTE_MASKS[(byte_width-1) as usize];
+            let to_write = (current & !mask) | ((val as u32) << ((byte_width-1)*8));
+            cpu::outd(0xCF8, addr);
+            cpu::outd(0xCFC, to_write);
+        }
+        Ok(())
     }
 }
 
