42102a982fb6fc1541eb1b0ca8bb98a12a0c1966
UnmappedStack <jake.steinburger0@gmail.com>
2025-10-18 12:57:52 +1100

    [acpi] start implementing uacpi kernel deps

--- kernel/src/acpi.rs
+++ kernel/src/acpi.rs
@@ -1,17 +1,62 @@
-use crate::{kernel::KERNEL/*, mem::paging*/};
-/*use alloc::sync::Arc;*/
+use crate::{kernel::KERNEL, cpu};
+use alloc::sync::Arc;
 
 struct AcpiKernelApi;
 
-//impl uacpi::kernel_api::KernelApi for AcpiKernelApi {
-//    fn raw_memory_read(&self, phys: uacpi::PhysAddr, byte_width: u8) -> Result<u64, Status> {
-//        paging::page_map_vmem(KERNEL);
-//    }
-//}
+impl uacpi::kernel_api::KernelApi for AcpiKernelApi {
+    unsafe fn raw_memory_read(&self, phys: uacpi::PhysAddr, byte_width: u8)
+                                                -> Result<u64, uacpi::Status> {
+        let kernel = KERNEL.lock();
+        unsafe {
+            match byte_width {
+                1 => Ok(*((phys.as_u64() + kernel.hhdm) as *const  u8) as u64),
+                2 => Ok(*((phys.as_u64() + kernel.hhdm) as *const u16) as u64),
+                4 => Ok(*((phys.as_u64() + kernel.hhdm) as *const u32) as u64),
+                8 => Ok(*((phys.as_u64() + kernel.hhdm) as *const u64) as u64),
+                _ => Err(uacpi::Status::InvalidArgument),
+            }
+        }
+    }
+    unsafe fn raw_memory_write(&self, phys: uacpi::PhysAddr, byte_width: u8, val: u64)
+                                                -> Result<(), uacpi::Status> {
+        let kernel = KERNEL.lock();
+        unsafe {
+            match byte_width {
+                1 => Ok(*((phys.as_u64() + kernel.hhdm) as *mut  u8) = val as u8 ),
+                2 => Ok(*((phys.as_u64() + kernel.hhdm) as *mut u16) = val as u16),
+                4 => Ok(*((phys.as_u64() + kernel.hhdm) as *mut u32) = val as u32),
+                8 => Ok(*((phys.as_u64() + kernel.hhdm) as *mut u64) = val as u64),
+                _ => Err(uacpi::Status::InvalidArgument),
+            }
+        }
+    }
+    unsafe fn raw_io_read(&self, addr: uacpi::IOAddr, byte_width: u8)
+                                                -> Result<u64, uacpi::Status> {
+        unsafe {
+            match byte_width {
+                1 => Ok(cpu::inb(addr.as_u64() as u16) as u64),
+                2 => Ok(cpu::inw(addr.as_u64() as u16) as u64),
+                4 => Ok(cpu::ind(addr.as_u64() as u16) as u64),
+                _ => Err(uacpi::Status::InvalidArgument),
+            }
+        }
+    }
+    unsafe fn raw_io_write(&self, addr: uacpi::IOAddr, byte_width: u8, val: u64)
+                                                -> Result<(), uacpi::Status> {
+        unsafe {
+            match byte_width {
+                1 => {cpu::outb(addr.as_u64() as u16, val as u8 ); Ok(())},
+                2 => {cpu::outw(addr.as_u64() as u16, val as u16); Ok(())},
+                4 => {cpu::outd(addr.as_u64() as u16, val as u32); Ok(())},
+                _ => Err(uacpi::Status::InvalidArgument),
+            }
+        }
+    }
+}
 
 pub fn init() {
     let kernel = KERNEL.lock();
-//    uacpi::kernel_api::set_kernel_api(Arc::new(AcpiKernelApi));
+    uacpi::kernel_api::set_kernel_api(Arc::new(AcpiKernelApi));
     let _s = uacpi::init(
         uacpi::PhysAddr::new(kernel.rsdp),
         uacpi::LogLevel::WARN,
--- kernel/src/cpu.rs
+++ kernel/src/cpu.rs
@@ -38,3 +38,43 @@ pub unsafe fn inb(port: u16) -> u8 {
     }
     ret
 }
+
+pub unsafe fn inw(port: u16) -> u16 {
+    let mut ret: u16;
+    unsafe {
+        asm!("in ax, dx",
+            in("dx") port,
+            out("ax") ret,
+        );
+    }
+    ret
+}
+
+pub unsafe fn outw(port: u16, val: u16) {
+    unsafe {
+        asm!("out dx, ax",
+            in("dx") port,
+            in("ax") val,
+        );
+    }
+}
+
+pub unsafe fn ind(port: u16) -> u32 {
+    let mut ret: u32;
+    unsafe {
+        asm!("in eax, dx",
+            in("dx") port,
+            out("eax") ret,
+        );
+    }
+    ret
+}
+
+pub unsafe fn outd(port: u16, val: u32) {
+    unsafe {
+        asm!("out dx, eax",
+            in("dx") port,
+            in("eax") val,
+        );
+    }
+}
