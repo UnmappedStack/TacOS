d89fc4b0275cac88b710cdb99c64a5257ebd46c9
UnmappedStack <jake.steinburger0@gmail.com>
2026-08-31 16:00:47 +1000

    [smp] make locks cross isa

--- kernel/include/lock.h
+++ kernel/include/lock.h
@@ -2,17 +2,10 @@
 #include <stdatomic.h>
 
 // TODO: this is unfair
-#if defined(__x86_64__)
 #define Spinlock atomic_flag
 #define spinlock_acquire(lock) \
     while (atomic_flag_test_and_set(lock)) { \
-        __builtin_ia32_pause(); \
+        PAUSE(); \
     }
 #define spinlock_release(lock) \
     atomic_flag_clear(lock);
-#elif defined(__riscv)
-#define Spinlock int
-// TODO: stub
-#define spinlock_acquire(lock) {(void)lock;}
-#define spinlock_release(lock) {(void)lock;}
-#endif
